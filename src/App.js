4eaf060e954694c44dc868fabcb8ba28cb2449c5
med.amine <mohamed.amine.aloui@gmail.com>
2021-09-14 00:31:38 +0100

    header component implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import React, { Component } from "react";
+import {BrowserRouter as Router,Route,Switch, Redirect} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
+import Header from "./components/Header/Header";
+import Register from "./views/Register";
+import Login from "./views/Login";
+import Todo from "./views/Todo";
+import Update from "./components/Update";
+import Protected from "./components/Protected";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+		<Header />
+		<Switch>
+			<Route path='/login'    component={Login} />
+			<Route path='/register' component={Register} />
+			<Route path='/todo'>
+				<Protected Cmp={Todo} />
+			</Route>
+			<Route exact path='/Update/:id'>
+				<Protected Cmp={Update} />
+			</Route>
+			<Route path='/'>
+				<Protected Cmp={Todo} />
+			</Route>
+		</Switch>
+	</Router>
   );
 }
 
